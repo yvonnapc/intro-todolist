34d8545c2150667fee07caa1ea0f27b00e6966f5
nic netzel and yvonna contreras <yvonnap.contreras@gmail.com>
2016-01-25 16:28:49 -0800

    Try to make clickable link work

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,33 @@ function Task (name) {
   this.name = name;
 }
 
-// $(document).ready(function() {
-//
-//
-//
-// });
+Task.prototype.completed = function(){
+  return this.name + " DONE";
+}
+
+
+
+
+$(document).ready(function() {
+
+
+
+ $("form#list").submit(function(event){
+   event.preventDefault();
+   var newTask = $("input#newTask").val();
+   var listedTask = new Task(newTask);
+
+   $(".spaceJam").append("<li><span class='clickable'>" + listedTask.name + "</span></li>");
+   $("#showList").show();
+   console.log(listedTask);
+
+
+ });
+
+ $(".clickable").click(function(event){
+   event.preventDefault();
+   return this.name.completed();
+   console.log(this.name.completed());
+   $(".spaceJam").append("<li><span class='clickable'>" + this.name.completed + "</span></li>");
+ })
+});
